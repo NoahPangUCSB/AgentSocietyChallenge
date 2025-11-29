94836fbb66a0b1a9f7244e774dbcfc7b23091493
Noah Pang <noahpang@cs.ucsb.edu>
2025-11-29 13:02:32 -0800

    test baseline

--- example/RecAgent_baseline.py
+++ example/RecAgent_baseline.py
@@ -179,7 +179,7 @@ if __name__ == "__main__":
 
     # Run evaluation
     # If you don't set the number of tasks, the simulator will run all tasks.
-    agent_outputs = simulator.run_simulation(number_of_tasks=None, enable_threading=True, max_workers=10)
+    agent_outputs = simulator.run_simulation(number_of_tasks=5, enable_threading=True, max_workers=10)
 
     # Evaluate the agent
     evaluation_results = simulator.evaluate()
